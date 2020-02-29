e82afd722cc3ce0024b1012531db85607ed4e769
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-02-29 22:44:05 +0530

    Update loader

--- lib/widgets/stream_loading.dart
+++ lib/widgets/stream_loading.dart
@@ -1,10 +1,11 @@
+import 'package:connectit_app/widgets/app_loader.dart';
 import 'package:flutter/material.dart';
 
 class StreamLoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CircularProgressIndicator(),
+      child: AppLoader(),
     );
   }
 }
